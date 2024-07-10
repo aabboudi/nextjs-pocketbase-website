5b25fdec1cee63bae4e8f07a276f05236ce53db5
aabboudi <abboudi.abdellah@gmail.com>
2024-07-10 22:58:23 +0100

    Removed commented code

--- pages/api/dbconn.js
+++ pages/api/dbconn.js
@@ -51,20 +51,3 @@ export default async function dbconn() {
     console.error('Error using session token with PocketBase:', error);
   }
 }
-
-/*
-import PocketBase from 'pocketbase';
-
-const url = 'https://poypoy.pockethost.io/';
-const client = new PocketBase(url);
-
-export default async function dbconn() {
-  try {
-    await client.admins.authWithPassword('abboudi.abdellah@gmail.com', 'poypoy123');
-    return client;
-  } catch (error) {
-    console.error('Error authenticating with PocketBase:', error);
-    throw error;
-  }
-}
-*/
